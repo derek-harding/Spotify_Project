7d009ebec302974074699d25b765ee9a9d0f0c9b
dhvols1 <dhvols1@aim.com>
2016-08-21 11:30:33 -0400

    1st Analysis
    Test of Jupyter to Github

--- DataPull.R
+++ DataPull.R
@@ -3,9 +3,11 @@ library(tidyr)
 library(dplyr)
 library(purrr)
 
+# Client and Secret ID's Keep Secret actually secret!
 clientID = 'fc1431c1d2b14344a39ad397695f098b'
 secret = '979fe2f89cdb4b85bfe31dc9703e0faa'
 
+# Token Stuff
 response = POST(
   'https://accounts.spotify.com/api/token',
   accept_json(),
@@ -17,15 +19,17 @@ response = POST(
 
 mytoken = content(response)$access_token
 
-
 HeaderValue <- paste0('Bearer ', mytoken)
 
+
+#Pull Playlist tracks from API
 URI = paste0('https://api.spotify.com/v1/users/dhvols1/playlists/18WUBxYcdwXN0dMiRdeUW6/tracks?limit=100')
 response2 <- GET(url = URI, add_headers(Authorization = HeaderValue))
 Tracks <- content(response2)
 
 Tracks2 <- Tracks$items
 
+#Put tracks into a data frame 
 Tracks2_df <- data.frame()
 for(i in 1:length(Tracks2)){
   Tracks_df <- data.frame(added_at = as.character(Tracks2[[i]]$added_at),
@@ -44,8 +48,9 @@ for(i in 1:length(Tracks2)){
   print(i)
   }
 
+
+# There is a limit of 100 tracks per call. This pulls the rest of the tracks
 offset <- nrow(Tracks2_df)
-HeaderValue <- paste0('Bearer ', mytoken)
 URI = paste0('https://api.spotify.com/v1/users/dhvols1/playlists/18WUBxYcdwXN0dMiRdeUW6/tracks?offset=',offset)
 response2 <- GET(url = URI, add_headers(Authorization = HeaderValue))
 Tracks <- content(response2)
@@ -68,3 +73,30 @@ for(i in 1:length(Tracks2)){
   Tracks2_df <- Tracks2_df %>% bind_rows(Tracks_df)
   print(i)
 }
+
+Audio_Features_df2 <- data.frame()
+for(i in 1:nrow(Tracks2_df)){
+  
+  URI = paste0('https://api.spotify.com/v1/audio-features?ids=',Tracks2_df$track_id[i])
+  response2 <- GET(url = URI, add_headers(Authorization = HeaderValue))
+  Audio_Features <- content(response2)
+  
+  Audio_Features_df <- data.frame(id = Audio_Features$audio_features[[1]]$id,
+                                  danceability = Audio_Features$audio_features[[1]]$danceability,
+                                  energy = Audio_Features$audio_features[[1]]$energy,
+                                  loudness = Audio_Features$audio_features[[1]]$loudness,
+                                  mode = Audio_Features$audio_features[[1]]$mode,
+                                  speechiness = Audio_Features$audio_features[[1]]$speechiness,
+                                  acousticness = Audio_Features$audio_features[[1]]$acousticness,
+                                  instrumentalness = Audio_Features$audio_features[[1]]$instrumentalness,
+                                  liveness = Audio_Features$audio_features[[1]]$liveness,
+                                  valence = Audio_Features$audio_features[[1]]$valence,
+                                  temp = Audio_Features$audio_features[[1]]$tempo,
+                                  time_signature = Audio_Features$audio_features[[1]]$time_signature)
+  Audio_Features_df2 <- Audio_Features_df2 %>%
+    bind_rows(Audio_Features_df)
+  print(i)
+}
+
+Full_Track_Data <- Tracks2_df %>%
+  left_join(Audio_Features_df2, by = c("track_id" = "id"))
